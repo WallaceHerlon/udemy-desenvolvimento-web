3c16abdac865b4f09e60acd87b284091f714b257
Wallace Herlon Silva <wherlon@hotmail.com>
2023-01-14 21:22:24 -0300

    Projeto 02: sistema administrativo - Criando menu lateral

--- projetos/02_sistema_administrativo/js/adm.js
+++ projetos/02_sistema_administrativo/js/adm.js
@@ -11,3 +11,13 @@ function dropMenu(selector) {
         dropdownMenu.classList.contains("active") ? dropdownMenu.classList.remove("active") : dropdownMenu.classList.add("active");
     });
 }
+
+// Sidebar Toggle / bars
+let sidebar = document.querySelector(".sidebar");
+let bars = document.querySelector(".bars");
+
+bars.addEventListener("click", () => {
+    sidebar.classList.contains("active") ? sidebar.classList.remove("active") : sidebar.classList.add("active");
+});
+
+window.matchMedia("(max-width: 768px)").matches ? sidebar.classList.remove("active") : sidebar.classList.add("active");
