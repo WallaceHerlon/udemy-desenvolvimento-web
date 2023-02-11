503be35b73b31dd5c155663d5b5856b43e84c5d6
Wallace Herlon Silva <wherlon@hotmail.com>
2023-02-10 23:42:15 -0300

    Projeto 03: Site PHP e MySQLi - Setup inicial

---
+++ projetos/03_site_php_mysqli/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="app/sts/assets/image/icon/favicon.ico" type="image/x-icon">
+        <title>Home</title>
+    </head>
+
+    <body>
+        <?php
+            echo "Bem vindo ao site...!"
+        ?>
+    </body>
+
+</html>
