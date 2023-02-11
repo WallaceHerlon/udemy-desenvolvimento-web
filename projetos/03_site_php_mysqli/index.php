5011f69272458a4751ca638b3f6c797198a4b955
Wallace Herlon Silva <wherlon@hotmail.com>
2023-02-11 20:26:12 -0300

    Projeto 03: Site PHP e MySQLi - URL customizada com PHP

--- projetos/03_site_php_mysqli/index.php
+++ projetos/03_site_php_mysqli/index.php
@@ -1,6 +1,23 @@
 <?php
-$url = filter_input(INPUT_GET, 'pagina', FILTER_DEFAULT);
+// Receber a url
+$url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
 var_dump($url);
+
+// Converter a string em arry
+$url_path = explode("/", $url);
+var_dump($url_path);
+
+// Verificar se existe a posição 1 no array
+if(isset($url_path['0'])){
+    $path_page = $url_path['0'];
+    var_dump($path_page);
+}
+
+// Verificar se existe a posição 2 no array
+if(isset($url_path['1'])){
+    $path_detail = $url_path['1'];
+    var_dump($path_detail);
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -15,9 +32,10 @@ var_dump($url);
 
 <body>
     <?php
-    if (!empty($url)) {
-        if (file_exists("app/sts/view/" . $url)) {
-            include "app/sts/view/" . $url;
+    // Verificar se tem o nome da página
+    if (!empty($path_page)) {
+        if (file_exists("app/sts/view/" . $path_page . ".php")) {
+            include "app/sts/view/" . $path_page . ".php";
         } else {
             include "app/sts/view/404.php";
         }
