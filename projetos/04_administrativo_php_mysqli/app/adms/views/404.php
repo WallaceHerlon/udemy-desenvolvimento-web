5916265d6aa1284394d324f9f96521fc3b7b1cc4
Wallace Herlon Silva <wherlon@hotmail.com>
2023-02-14 20:54:07 -0300

    Projeto 04: Administrativo PHP e MySQLi - URL amigável com PHP

---
+++ projetos/04_administrativo_php_mysqli/app/adms/views/404.php
@@ -0,0 +1,2 @@
+<?php
+echo "Erro 404: Ops... O link que você tentou acessar não está disponível ou foi engolido por um buraco negro...";
--- projetos/04_administrativo_php_mysqli/index.php
+++ projetos/04_administrativo_php_mysqli/index.php
@@ -1,5 +1,29 @@
 <?php
-$path_page = "home";
+
+// Receber a url
+$url = filter_input(INPUT_GET, "url", FILTER_DEFAULT);
+// var_dump($url);
+
+// Converte a string em array
+$url_path = explode("/", $url);
+// var_dump($url_path);
+
+// Verificar se existe a posicao 1 no array
+if((isset($url_path['0'])) and (!empty($url_path['0']))){
+    $path_page = $url_path['0'];
+}else{
+    $path_page = "login";
+}
+// var_dump($path_page);
+
+// Verificar se existe a posicao 2 no array
+if((isset($url_path['1'])) and (!empty($url_path['1']))){
+    $path_detail = $url_path['1'];
+}else{
+    $path_detail = "";
+}
+// var_dump($path_detail);
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -15,7 +39,7 @@ $path_page = "home";
         if (file_exists("app/adms/views/" . $path_page . ".php")) {
             include_once "app/adms/views/" . $path_page . ".php";
         } else {
-            include_once "app/adms/views/login.php";
+            include_once "app/adms/views/404.php";
         }
     } else {
         include_once "app/adms/views/login.php";
