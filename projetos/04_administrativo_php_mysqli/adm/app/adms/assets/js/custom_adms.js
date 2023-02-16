f6a1bd697017644c22c3d57bae8d6c50fc68812c
Wallace Herlon Silva <wherlon@hotmail.com>
2023-02-16 19:59:40 -0300

    Projeto 04: Administrativo PHP e MySQLi - Validar formulário com Javascript novo usuário

--- projetos/04_administrativo_php_mysqli/adm/app/adms/assets/js/custom_adms.js
+++ projetos/04_administrativo_php_mysqli/adm/app/adms/assets/js/custom_adms.js
@@ -26,3 +26,66 @@ if(formLogin){
         }
     });
 }
+
+// Receber o formulario novo usuario no login
+const formNewUser = document.getElementById("form-new-user");
+
+// Verificar se existe o formulario novo usuario no login
+if(formNewUser){
+    // Aguardar o evento submit, aguardar o usuario clicar no botao cadastrar do formulario novo usuario no login
+    formNewUser.addEventListener("submit", async(e) => {
+        // Receber o valor do campo
+        var name = document.querySelector("#name").value;
+        
+        // Verificar se o campo esta vazio
+        if(name === ""){
+            e.preventDefault();
+            document.getElementById("msg").innerHTML = "<p style='color: #f00;'>Erro: Necessário preencher o campo nome!</p>";
+            return;
+        }
+        
+        // Receber o valor do campo
+        var email = document.querySelector("#email").value;
+        
+        // Verificar se o campo esta vazio
+        if(email === ""){
+            e.preventDefault();
+            document.getElementById("msg").innerHTML = "<p style='color: #f00;'>Erro: Necessário preencher o campo e-mail!</p>";
+            return;
+        }
+        
+        // Receber o valor do campo
+        var password = document.querySelector("#password").value;
+        
+        // Verificar se o campo esta vazio
+        if(password === ""){
+            e.preventDefault();
+            document.getElementById("msg").innerHTML = "<p style='color: #f00;'>Erro: Necessário preencher o campo senha!</p>";
+            return;
+        }
+
+        // Verificar se o campo senha possui 6 caracteres
+        if(password.length < 6){
+            e.preventDefault();
+            document.getElementById("msg").innerHTML = "<p style='color: #f00;'>Erro: A senha deve ter no mínimo 6 caracteres!</p>";
+            return;
+        }
+
+        // Verificar se o campo senha nao possui numeros repetidos
+        if (password.match(/([1-9]+)\1{1,}/)) {
+            e.preventDefault();
+            document.getElementById("msg").innerHTML = "<p style='color: #f00;'>Erro: A senha não deve ter número repetido!</p>";
+            return;
+        }
+
+        // Verificar se o campo senha possui letras
+        if (!password.match(/[A-Za-z]/)) {
+            e.preventDefault();
+            document.getElementById("msg").innerHTML = "<p style='color: #f00;'>Erro: A senha deve ter pelo menos uma letra!</p>";
+            return;
+        }else{
+            document.getElementById("msg").innerHTML = "<p></p>";
+            return;
+        }
+    });
+}
